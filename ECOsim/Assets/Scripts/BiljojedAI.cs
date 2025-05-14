e099f648ccb8d2cf29bc85cdc70a0b74e024afed
pijera <petar200706@gmail.com>
2025-05-14 12:32:15 +0200

    Mogu da piju i da jedu

--- ECOsim/Assets/Scripts/BiljojedAI.cs
+++ ECOsim/Assets/Scripts/BiljojedAI.cs
@@ -18,18 +18,33 @@ public class BiljojedAI : MonoBehaviour
     private bool hasTarget = false;    
     
     private bool isWaiting = false;
+    private bool isInteracting = false;
+
+    public Vector2 maxBounds;
+    public Vector2 minBounds;
     void Update()
     {
         if (isWaiting)return;
+        if (isInteracting)return;
+        
         hunger += Time.deltaTime * hungerRate;
         thirst += Time.deltaTime * thirstRate;
 
         GameObject target = FindTarget();
-        if (target != null && Vector2.Distance(transform.position, targetPosition) < 0.2f)
+        
+        if (target != null)
         {
+            float dist = Vector2.Distance(transform.position, target.transform.position);
+
+            if (dist < 1.5f)
+            {
+                StartCoroutine(Interacting(target));
+                return;
+            }
             targetPosition = target.transform.position;
             hasTarget = true;
         }
+        
         if (!hasTarget || Vector2.Distance(transform.position, targetPosition) < 0.2f)
         {
             StartCoroutine(WaitBeforGoing());
@@ -73,9 +88,23 @@ public class BiljojedAI : MonoBehaviour
         float angle = Random.Range(0f, Mathf.PI * 2f);
         float radius = Random.Range(0.3f, sightRange);
         Vector2 offset = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle))*radius;
-        return (Vector2)transform.position + offset;
+        Vector2 point = (Vector2)transform.position + offset;
+        return new Vector2(
+            Math.Clamp(point.x,minBounds.x,maxBounds.x),
+            Math.Clamp(point.y,minBounds.y,maxBounds.y)
+            );
     }
 
+    IEnumerator Interacting(GameObject target)
+    {
+        isInteracting = true;
+        hasTarget = false;
+        yield return new WaitForSecondsRealtime(1f);
+        if (target.CompareTag("Food")) hunger = 0;
+        if (target.CompareTag("Water")) thirst = 0;
+        isInteracting = false;
+    }
+    
     IEnumerator WaitBeforGoing()
     {
         isWaiting = true;
@@ -88,17 +117,18 @@ public class BiljojedAI : MonoBehaviour
 
     private void MoveTowardsTarget()
     {
-        float dis = Vector2.Distance(transform.position, targetPosition);
-        if (dis > 0.2f)
-        {
-            Vector2 pos = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed*Time.deltaTime);
-            transform.position = pos;
-        }
+        Vector2 pos = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed*Time.deltaTime);
+        
+        pos.x = Mathf.Clamp(pos.x, minBounds.x, maxBounds.x);
+        pos.y = Mathf.Clamp(pos.y, minBounds.y, maxBounds.y);
+        
+        transform.position = pos;
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.DrawWireSphere(transform.position, 0.5f);
     }
 }
